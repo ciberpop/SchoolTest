8ec6b0b07289d21e2382e74d572d73b27d5d166b
ciberpop <jelrainverner@gmail.com>
2020-07-07 13:10:33 +0300

    Remove field 'students' from table 'group'

--- src/validators/group.validator.js
+++ src/validators/group.validator.js
@@ -2,7 +2,5 @@ const Joi = require('joi');
 
 module.exports = Joi.object().keys({
   title: Joi.string().trim().min(2).max(15)
-    .required(),
-  students: Joi.array().items(Joi.object({ studentId: Joi.string() }))
     .required()
 });
